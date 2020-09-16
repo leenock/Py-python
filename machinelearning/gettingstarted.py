7514d73ec6d28f010678cb3660432ca56e11dbc8
leenock <leenock176@gmail.com>
2020-09-16 12:41:33 +0300

    machine learning

---
+++ machinelearning/gettingstarted.py
@@ -0,0 +1,34 @@
+# In this tutorial we will go back to mathematics and study statistics, and how to calculate important numbers based on data sets.
+#
+# We will also learn how to use various Python modules to get the answers we need.
+#
+# And we will learn how to make functions that are able to predict the outcome based on what we have learned.
+
+# In the mind of a computer, a data set is any collection of data. It can be anything from an array to a complete database.
+#
+# Example of an array:
+#
+# [99,86,87,88,111,86,103,87,94,78,77,85,86]
+
+
+# Data Types
+# To analyze data, it is important to know what type of data we are dealing with.
+#
+# We can split the data types into three main categories:
+#
+# Numerical
+# Categorical
+# Ordinal
+# Numerical data are numbers, and can be split into two numerical categories:
+#
+# Discrete Data
+# - numbers that are limited to integers. Example: The number of cars passing by.
+# Continuous Data
+# - numbers that are of infinite value. Example: The price of an item, or the size of an item
+# Categorical data are values that cannot be measured up against each other. Example: a color value, or any yes/no values.
+#
+# Ordinal data are like categorical data, but can be measured up against each other. Example: school grades where A is better than B and so on.
+#
+# By knowing the data type of your data source, you will be able to know what technique to use when analyzing them.
+#
+# You will learn more about statistics and analyzing data in the next chapters.
